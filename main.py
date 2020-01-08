21738b987441e2765e631cc972a1a1c31a228550
Jacob Hillock <jacobhillock@gmail.com>
2020-01-08 12:20:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,107 @@
 from pyTwistyScrambler import scrambler333
+import pycuber as pc
+import pygame
+import sys
 
-scramble = scrambler333.get_WCA_scramble()
-scramble = scramble.split(" ")
-print(scramble)
+def generate():
+    # create a random scramble according the the WCA scramble algorithm
+    scramble = scrambler333.get_WCA_scramble()
+    # scramble = scramble.split(" ")
+    print(scramble)
+
+
+    # Create a Cube object
+    mycube = pc.Cube()
+    # Scramble the cube according to the wca scamble
+    mycube(scramble)
+
+    # convert cube into a sctring for manipulation
+    cube_state = str(mycube)
+    # split cube string into a grid list
+    grid = [[i] for i in cube_state.split("\n")]
+    grid.pop(-1)
+
+
+    # make sure all rows of grid are the same length
+    for i in range(len(grid)):
+        if(len(grid[i][0]) < 36):
+            grid[i][0] += " "*18
+
+    # make grid truly 2d
+    for i in range(len(grid)):
+        temp = []
+        for j in range(12):
+            temp.append(grid[i][0][1 + j*3])
+        # print(temp)
+        grid[i] = temp
+
+    # make scramble as if white is on top (swap orange for red, and yellow for white)
+    for r in range(len(grid)):
+        for c in range(len(grid[r])):
+            if grid[r][c] == 'o':
+                grid[r][c] = 'r'
+            elif grid[r][c] == 'r':
+                grid[r][c] = 'o'
+            elif grid[r][c] == 'w':
+                grid[r][c] = 'y'
+            elif grid[r][c] == 'y':
+                grid[r][c] = 'w'
+    
+    # print grid (for debugging)
+    # for r in grid:
+    #     print(r)
+    
+    return grid
+
+def draw(grid):
+    # https://www.pygame.org/docs/tut/PygameIntro.html
+    # startup for gui
+    pygame.init()
+
+    pixel_size = 40
+
+    height = len(grid) * pixel_size + 1
+    width = len(grid[0]) * pixel_size
+    size = width, height
+    screen = pygame.display.set_mode(size)
+    black = pygame.Color(0, 0, 0)
+    screen.fill(black)
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            cl = grid[i][j]
+            color = pygame.Color(0, 0, 0)
+
+            if cl == 'w':
+                color = pygame.Color(255, 255, 255)
+            elif cl == 'y':
+                color = pygame.Color(255, 255, 0)
+            elif cl == 'r':
+                color = pygame.Color(255, 0, 0)
+            elif cl == 'o':
+                color = pygame.Color(255, 165, 0)
+            elif cl == 'b':
+                color = pygame.Color(0, 0, 255)
+            elif cl == 'g':
+                color = pygame.Color(0, 255, 0)
+            
+            x = i * pixel_size
+            y = j * pixel_size
+            l = pixel_size - 1
+            rect = pygame.Rect(y, x, l, l)
+            pygame.draw.rect(screen, color, rect)
+
+    pygame.display.update()
+
+
+
+def main():
+    grid = generate()
+    draw(grid)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+if __name__ == "__main__":
+    main()
